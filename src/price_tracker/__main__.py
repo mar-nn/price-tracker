3aefd0084d479ff125caf3c720622713457f1e00
João Medeiros Miranda <up202207769@edu.fe.up.pt>
2026-03-13 11:00:25 +0000

    transform __main__.py into an openaicaller() function

--- src/price_tracker/__main__.py
+++ src/price_tracker/__main__.py
@@ -1,14 +1,15 @@
 from openai import OpenAI
 
-client = OpenAI()
+def openaicaller():
+    client = OpenAI()
 
-response = client.responses.create(
-    model="gpt-4o-mini",
-    input=[
-        {"role": "user", "content": "knock knock."},
-        {"role": "assistant", "content": "Who's there?"},
-        {"role": "user", "content": "Orange."},
-    ],
-)
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=[
+            {"role": "user", "content": "knock knock."},
+            {"role": "assistant", "content": "Who's there?"},
+            {"role": "user", "content": "Orange."},
+        ],
+    )
 
-print(response.output_text)
+    print(response.output_text)
