ae5a767a1077510ab1263dcaafad41b2a84e2ad9
João Medeiros Miranda <up202207769@edu.fe.up.pt>
2026-03-15 23:31:35 +0000

    create tests folder along with test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch, MagicMock
+from price_tracker.__main__ import openaicaller
+
+
+@patch("price_tracker.__main__.OpenAI")
+def test_openaicaller(mock_openai):
+
+    mock_client = MagicMock()
+    mock_openai.return_value = mock_client
+
+    mock_response = MagicMock()
+    mock_response.output_text = "Orange who?"
+
+    mock_client.responses.create.return_value = mock_response
+
+    result = openaicaller()
+
+    assert result == "Orange who?"
+    mock_client.responses.create.assert_called_once()
