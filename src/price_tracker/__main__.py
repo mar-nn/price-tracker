52e78ba9e53bb3a98b5f5c01fbddec0988859252
João Medeiros Miranda <up202207769@edu.fe.up.pt>
2026-04-06 16:07:21 +0100

    add --url and --model to parser

--- src/price_tracker/__main__.py
+++ src/price_tracker/__main__.py
@@ -1,10 +1,18 @@
 import argparse
 import requests
 import os
+
 from openai import OpenAI
-from openai_client import openai_caller, price_extractor
+from price_tracker.openai_client import (openai_caller, price_extractor)
 
 MODEL_NAME = os.getenv("OPENAI_MODEL","gpt-4o-mini")
+
+parser = argparse.ArgumentParser(description = "extract price from URL")
+
+parser.add_argument("--url", required = True, help = "URL of the product page")
+
+parser.add_argument("--model", default = MODEL_NAME, help = "OpenAI model to use")
+
 
 def main():
     #result = openai_caller()
