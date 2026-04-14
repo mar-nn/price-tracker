07d79cc07ae7cbeca3df9c870f1d084370d450fc
João Medeiros Miranda <up202207769@edu.fe.up.pt>
2026-04-14 15:29:31 +0100

    create cli() function for parser

--- src/price_tracker/__main__.py
+++ src/price_tracker/__main__.py
@@ -7,16 +7,7 @@ from price_tracker.openai_client import price_extractor
 
 MODEL_NAME = os.getenv("OPENAI_MODEL","gpt-4o-mini")
 
-parser = argparse.ArgumentParser(description = "extract price from URL")
-
-parser.add_argument("--url", required = True, help = "URL of the product page")
-
-parser.add_argument("--model", default = MODEL_NAME, help = "OpenAI model to use")
-
-
 def main(url: str, model: str):
-
-
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -28,7 +19,19 @@ def main(url: str, model: str):
     result = price_extractor(html, model)
 
     print(result)
+    
+
+def cli():
+    parser = argparse.ArgumentParser(description = "extract price from URL")
+
+    parser.add_argument("--url", required = True, help = "URL of the product page")
+
+    parser.add_argument("--model", default = MODEL_NAME, help = "OpenAI model to use")
+
+    args = parser.parse_args()
+
+    main(url=args.url, model=args.model)
+
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    main(url=args.url, model=args.model)
+    cli()
