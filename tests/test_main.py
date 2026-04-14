8d0ac83e50454a1171b9f8d20d8ec1f841c4e082
João Medeiros Miranda <up202207769@edu.fe.up.pt>
2026-04-14 16:19:16 +0100

    create unit test for price_extractor function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch, MagicMock
-from price_tracker.openai_client import openai_caller
+from price_tracker.openai_client import (openai_caller, price_extractor)
 
 
 @patch("price_tracker.openai_client.OpenAI")
@@ -16,3 +16,14 @@ def test_openai_caller(mock_openai):
 
     assert result == "Orange who?"
     mock_client.responses.create.assert_called_once()
+
+
+@patch("price_tracker.openai_client.OpenAI")
+def test_price_extractor(mock_openai):
+    mock_client = MagicMock()
+    mock_client.responses.create.return_value.output_text = "15 000 €"
+    mock_openai.return_value = mock_client
+
+    result = price_extractor("<html>Tung Tung Tung Sahur T-Shirt 15 000 €</html>", "gpt-4o-mini")
+
+    assert "15 000 €" in result
